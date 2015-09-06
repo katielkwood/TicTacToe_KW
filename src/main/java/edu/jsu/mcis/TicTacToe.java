4d7c6bd560ad6a741e09462c607a4c84deba4a16
katielkwood <katielkwood@gmail.com>
2015-09-06 13:08:31 -0500

    first 4 tests passing

--- src/main/java/edu/jsu/mcis/TicTacToe.java
+++ src/main/java/edu/jsu/mcis/TicTacToe.java
@@ -2,15 +2,30 @@ package edu.jsu.mcis;
 
 public class TicTacToe {
 
-
-
-	int[][] board;
+	//private TicTacToe t;
+	//public enum Mark {XMARK, OMARK, EMPTY}
+	private char[][] board = new char[3][3];
+	
+	public TicTacToe(){
+		for (int row = 0; row < 3; row++){
+			for (int col = 0; col < 3; col++){
+				board[row][col] = ' ';
+			}
+		}
+	}
+	public char getMark(int row, int col){
+		return board[row][col];
+	}
+	
+	public void setMark(int row, int col, char mark){
+		if (board[row][col] == ' '){
+			board[row][col] = mark;
+		}
+	}
 
 	public static void main(String[] args) {
 		
 	}
-	//need to make an array
-	public char getMark(int row, int col){
-		
-	}
+	
+	
 }
--- src/test/java/edu/jsu/mcis/TicTacToeTest.java
+++ src/test/java/edu/jsu/mcis/TicTacToeTest.java
@@ -2,14 +2,18 @@ package edu.jsu.mcis;
 
 import org.junit.*;
 import static org.junit.Assert.*;
+import java.util.Random;
 
 public class TicTacToeTest {
-	//make an object t
+
+	//private TicTacToe t;
+	//public enum Mark {XMARK, OMARK, EMPTY};
+	
 	/*@Before 
 	public void setUp(){
-		TicTacToe t = new TicTacToe;
-	}
-	*/
+		TicTacToe t = new TicTacToe();
+	}*/
+	
 	@Test
 	public void testInitialBoardIsEmpty() {
 		TicTacToe t = new TicTacToe();
@@ -22,17 +26,27 @@ public class TicTacToeTest {
 	
 	@Test
 	public void testMarkXInUpperRightCorner() {
-		assertTrue(false);
+		TicTacToe t = new TicTacToe();
+		t.setMark(0, 2, 'X');
+		assertEquals('X', t.getMark(0, 2)); 
 	}
 	
 	@Test
 	public void testMarkOInBottomLeftCorner() {
-		assertTrue(false);
+		TicTacToe t = new TicTacToe();
+		t.setMark(2, 0, 'O');
+		assertEquals('O', t.getMark(2, 0));
 	}
 	
 	@Test
 	public void testUnableToMarkOverExistingMark() {
-		assertTrue(false);
+		TicTacToe t = new TicTacToe();
+		Random randInt = new Random();
+		int row = randInt.nextInt(3);
+		int col = randInt.nextInt(3);
+		t.setMark(row, col, 'O');
+		t.setMark(row, col, 'X');
+		assertEquals('O', t.getMark(row, col));
 	}
 	
 	@Test
